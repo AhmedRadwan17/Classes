e052c551446c76b6a9a855f6eaae5534164c3c0e
Ahmed Radwan <marimradwan922@gmail.com>
2025-02-02 05:12:04 +0200

    Create Main.cpp

---
+++ class_String/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "clsString.h"
+using namespace std;
+int main() { 
+clsString String1;
+
+String1.SetValue("Ali Radwan");
+  
+cout<<"String1: "<<String1.GetValue()<<endl; 
+cout<<"CountLetters: "<<String1.CountWords()<<endl;
+cout<<"SmallCase: "<<String1.LowerCase()<<endl;
+cout<<"UpeerCase: "<<String1.UpperCase()<<endl;
+cout <<"ReverseString: " << String1.ReverseString(String1.GetValue()) << endl;
+cout<<"Vowels: "; 
+String1.PrintVowels();
+cout<<"\nCountVowels: "<<String1.CountVowels()<<endl;
+}
